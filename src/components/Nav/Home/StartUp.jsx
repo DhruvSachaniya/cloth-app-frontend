038c83c0e0b366aaca785c0789c9193b25399336
Dhruv <sachaniyadhruv@email.com>
2023-10-05 11:34:34 +0530

    make some changes in login page

--- src/components/Nav/Home/StartUp.jsx
+++ src/components/Nav/Home/StartUp.jsx
@@ -1,28 +1,46 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 export default function LoginPage () {
     return(
         <div className="Login-back">
-            <div className="Login-form">
-                <div className="Login-heading">
+            <div className="grid-container">
+                <div className="grid-item Login-heading">
                     <h1>Welcome!</h1>
-                    <p>this is a e comerce website</p>
+                    <p>An online commerce platform offering a wide range of products, seamless shopping experiences, and secure transactions for all your shopping needs.</p>
                 </div>
-                <div className="form">
-                    {/* <form> */}
+                <div className="grid-item">
+                    <form className="login-form">
                         <h1>Login</h1>
-                        <input
-                        type="text"
-                        name="username"
-                        placeholder="username"
+                        <TextField
+                        className='customTextField ' 
+                        id="outlined-basic" 
+                        label="Username" 
+                        variant="outlined" 
+                        name='username'
+                        color="secondary"
+                        InputProps={{
+                            style: {
+                            color: '#FE7BE5',
+                            backgroundColor: 'transparent',
+                            
+                            },
+                        }}
                         />
+
                         <br/>
-                        <input
+
+                        <TextField
+                        id="outlined-password-input"
+                        label="Password"
                         type="password"
-                        name="password"
-                        placeholder="password"
+                        autoComplete="current-password"
+                        color="secondary"
                         />
                         <br/>
-                        <button type="submit">Login</button>
-                    {/* </form> */}
+
+                        <Button variant="contained" color="secondary">Login</Button>
+                    </form>
                 </div>
             </div>
         </div>
