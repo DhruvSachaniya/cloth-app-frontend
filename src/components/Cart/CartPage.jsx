65ad9691b6f100cf4edd27d4d358424e25428bf5
Dhruv <sachaniyadhruv@email.com>
2023-10-31 18:32:24 +0530

    update in cartpage

--- src/components/Cart/CartPage.jsx
+++ src/components/Cart/CartPage.jsx
@@ -3,23 +3,20 @@ import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 export default function CartProductPage() {
-
     const [GetData, SetData] = useState(null);
     const [GetProductCart, SetProductCart] = useState([]);
 
     const mystyle = {
         padding: "10px",
         display: "grid",
-        gridTemplateColumns: "1fr",   
-        alignItems: "center",       
-        justifyItems: "center",    
+        gridTemplateColumns: "1fr",
+        alignItems: "center",
+        justifyItems: "center",
     };
 
     const img_style = {
-        height: "200px"
+        height: "200px",
     };
-
-    
 
     useEffect(() => {
         try {
@@ -28,66 +25,77 @@ export default function CartProductPage() {
                     url: "/cart",
                     method: "get",
                     headers: {
-                        "Authorization": localStorage.getItem("jwt_token")
-                    }
-                })
-                if (response) {
-                    if (response.status === 200) {
-                        SetData(response.data);
-                        
-                        const response_2 = GetData.items.map((productId) => {
-                            return axios({
-                                url: `/productinfo/${productId}`,
-                                method: "get",
-                                headers: {
-                                    "Authorization": localStorage.getItem("jwt_token")
-                                }
-                            });
-                        });
-                        Promise.all(response_2)
-                            .then((ProductResponse) => {
-                                const productData = ProductResponse.map((res) => res.data);
-                                SetProductCart(productData);
-                            })
-                            .catch((error) => {
-                                toast.error(error)
-                            });
-                    }
+                        "Authorization": localStorage.getItem("jwt_token"),
+                    },
+                });
+                if (response && response.status === 200) {
+                    SetData(response.data);
                 }
             }
             fetchCart();
         } catch (error) {
             toast.error(error);
         }
-    }, [GetData]);
+    }, []);
 
+    useEffect(() => {
+        if (GetData) {
+            const response_2 = GetData.items.map((productId) => {
+                return axios({
+                    url: `/productinfo/${productId}`,
+                    method: "get",
+                    headers: {
+                        "Authorization": localStorage.getItem("jwt_token"),
+                    },
+                });
+            });
+            Promise.all(response_2)
+                .then((ProductResponse) => {
+                    const productData = ProductResponse.map((res) => res.data);
+                    SetProductCart(productData);
+                })
+                .catch((error) => {
+                    toast.error(error);
+                });
+        }
+    }, [GetData]);
 
     return (
         <div className="cart-container">
-        {GetData ? (
-            <div className="cart-h1">
-                <h2>Shopping Cart</h2>
-                <hr />
-                {GetProductCart.map((product, index) => (
-                <div className="cart-grid" key={GetData.items[index]}>
-                    <div className="cart-img">
-                        <img style={img_style} src={"http://localhost:5000/"+ product.fileURL} alt={product.title} />
-                    </div>
-                    <div>
-                        <h1>{product.title}</h1>
+            {GetData ? (
+                <div className="cart-h1">
+                    <h2>Shopping Cart</h2>
+                    <hr />
+                    {GetProductCart.map((product, index) => (
+                        <div className="cart-grid" key={GetData.items[index]}>
+                            <div className="cart-img">
+                                <img style={img_style} src={`http://localhost:5000/${product.fileURL}`} alt={product.title} />
+                            </div>
+                            <div>
+                                <h1 className="item-name">{product.title}</h1> 
+                                <p>{product.description}</p>
+                                <p className="item-price">$ {product.price}</p>
+                                {/* quantity */}
+                                {/* delete option */}
+                                {/* link to product page */}
+                            </div>
+                        </div>
+                    ))}
+                    <hr />
+                    <div className="cart-h2">
+                        <h3>Subtotal({GetData.items.length} items): ${GetData.subtotal}</h3>
+                        <button>Checkout</button>
                     </div>
                 </div>
-                ))}
-            </div>
-        ) : (
-            <div className="cart-h1">
-                <h2>Shopping Cart</h2>
-                <hr />
-                <div className="cart-grid" style={mystyle}>
-                    <h1>your cart is empty!</h1>
+            ) : (
+                <div className="cart-h1">
+                    <h2>Shopping Cart</h2>
+                    <hr />
+                    <div className="cart-grid" style={mystyle}>
+                        <h1>your cart is empty!</h1>
+                    </div>
                 </div>
-            </div>
-        )}
+            )}
         </div>
     );
 }
